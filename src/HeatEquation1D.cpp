75e2073256a7449d356a7a36ab6eba191850393e
Jonas McCandless <mccandless.jonas@gmail.com>
2025-09-30 12:56:00 +0200

    Check diffusion constant

--- src/HeatEquation1D.cpp
+++ src/HeatEquation1D.cpp
@@ -1,11 +1,18 @@
 #include "HeatEquation1D.h"
+#include <stdexcept>
 
 /**
  * @brief Construct a HeatEquation1D object.
  * 
  * This class stores the diffusion coefficient for later use by solvers.
  */
-HeatEquation1D::HeatEquation1D(double alpha_) : alpha(alpha_) {}
+HeatEquation1D::HeatEquation1D(double alpha_) : alpha(alpha_)
+{
+    if (!(alpha_ > 0.0)) 
+    {
+        throw std::invalid_argument("HeatEquation1D: The diffusion constant must be > 0.");
+    }
+}
 
 /**
  * @brief Return the diffusion coefficient. 
